efc836b3ac6ce49f95c12aaa6dc7568986cecc3b
Joa0DeL1ma <lisboajoaodelima@hotmail.com>
2024-10-16 08:41:26 -0300

    Working on home screen

--- app/src/main/java/com/example/flatflow/home/HomeScreen.kt
+++ app/src/main/java/com/example/flatflow/home/HomeScreen.kt
@@ -1,19 +1,29 @@
 package com.example.flatflow.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.flatflow.R
 import com.example.flatflow.home.widgets.HomeTopAppBar
 
 @Suppress("ktlint:standard:function-naming")
@@ -27,13 +37,83 @@ fun HomeScreen() {
                 .padding(start = 16.dp, top = 12.dp, end = 16.dp),
     ) {
         HomeTopAppBar(modifier = Modifier.padding(bottom = 16.dp))
-        Column(modifier = Modifier
-            .clip(RoundedCornerShape(topStart = 36.dp, topEnd = 36.dp))
-            .fillMaxWidth()
-            .heightIn(min = 300.dp)
-            .background(Color.White)
+        Column(
+            modifier =
+                Modifier
+                    .clip(RoundedCornerShape(topStart = 36.dp, topEnd = 36.dp))
+                    .fillMaxWidth()
+                    .heightIn(min = 300.dp)
+                    .background(Color.White),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
             ) {
-            IconButton(onClick = { /*TODO*/ }) {
+                IconButton(
+                    content = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_delete),
+                            contentDescription = "Touch to delete card from bulletin board",
+                            tint = Color.Gray,
+                        )
+                    },
+                    onClick = { /*TODO*/ },
+                )
+                Text(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    text = "Bulletin Board",
+                )
+                IconButton(
+                    content = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_add),
+                            contentDescription = "Touch to add a card to the bulletin board",
+                            tint = Color.Gray,
+                        )
+                    },
+                    onClick = { /*TODO*/ },
+                )
+            }
+        }
+        Column(
+            modifier =
+                Modifier
+                    .clip(RoundedCornerShape(16.dp))
+                    .height(150.dp)
+                    .padding(top = 16.dp)
+                    .fillMaxWidth()
+                    .background(Color.White),
+        ) {
+            Row(
+                modifier = Modifier.padding(start = 12.dp).fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(fontSize = 16.sp, fontWeight = FontWeight.Bold, text = "Bills Calendar")
+                Row {
+                    IconButton(
+                        content = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_delete),
+                                contentDescription = "Touch to delete card from bulletin board",
+                                tint = Color.Gray,
+                            )
+                        },
+                        onClick = { /*TODO*/ },
+                    )
+                    IconButton(
+                        content = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_add),
+                                contentDescription = "Touch to add a card to the bulletin board",
+                                tint = Color.Gray,
+                            )
+                        },
+                        onClick = { /*TODO*/ },
+                    )
+                }
             }
         }
     }
